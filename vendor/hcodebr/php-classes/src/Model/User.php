4c02a5d0acf64e0f61e1bf6e3c2a5c99030f3a04
Raul Silas <raulsilas22@gmail.com>
2018-12-25 17:21:46 -0200

    Recuperacao de senha - Site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -219,12 +219,18 @@ class User extends Model {
              $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
              $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
              $result = base64_encode($iv.$code);
-             if ($inadmin === true) {
-                 $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
-             } else {
-                 $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$result";
-             } 
-             $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
+			 
+			 if ($inadmin === true) {
+			
+				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
+			 
+			} else {
+			
+				$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$result";
+			
+			} 
+			
+			$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
                  "name"=>$data['desperson'],
                  "link"=>$link
              )); 
